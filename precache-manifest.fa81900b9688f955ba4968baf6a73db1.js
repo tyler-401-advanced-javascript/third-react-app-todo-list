5e104fc58691231c37ee3a604f5d444cecb06ce9
Tyler <tylersayvetz@gmail.com>
2020-03-23 16:46:58 -0700

    Updates

---
+++ precache-manifest.fa81900b9688f955ba4968baf6a73db1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a2c9bc0e6a0718695185718d8ac9ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceeafa5dbf06ada022cc",
+    "url": "/static/css/main.f0cb072a.chunk.css"
+  },
+  {
+    "revision": "520b36f2dba7e763ea6b",
+    "url": "/static/js/2.34624130.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.34624130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceeafa5dbf06ada022cc",
+    "url": "/static/js/main.34af6de1.chunk.js"
+  },
+  {
+    "revision": "b2a359c2ea95e1cdcb85",
+    "url": "/static/js/runtime-main.5d61fd10.js"
+  }
+]);
